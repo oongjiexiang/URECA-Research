bf118571d11abbfad91ce2298702fdb6768acdc9
oongjiexiang <oongjiexiang@yahoo.com>
2021-01-09 23:18:57 +0800

    complete script to automate scraping of idx file

--- extract.sh
+++ extract.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-read txt_file
+read idx_file;
+curl $idx_file | grep -E "^4[[:space:]]" | 
 awk -v home_link="https://www.sec.gov/Archives/" 'BEGIN{ORS=""; OFS=", "}
     {fieldNum = NF;
     for(i=2;i<fieldNum-2;i++) print $(i) " "
     print $(fieldNum-2)
-    print ", " $(fieldNum - 1), home_link $(fieldNum) "\n"}' "$txt_file" | 
+    print ", " $(fieldNum - 1), home_link $(fieldNum) "\n"}' | 
 while IFS=, read -r company date link; do
-    xml_file=`curl $link | sed -ne 's/<FILENAME>\(.*\)/\1/p' `
+    xml_file=`curl -s $link | sed -ne 's/<FILENAME>\(.*\)/\1/p' `
     temp=`echo $link | sed -e 's/-//g'`
-    new_link=`echo $temp | sed -e "s/.txt/\/$xml_file/"`
+    new_link=`echo $temp | sed -e 's|.txt|/$xml_file|'`
     echo $company "," $date "," $new_link
 done >buffer2.txt
 # read csv https://stackoverflow.com/questions/4286469/how-to-parse-a-csv-file-in-bash
